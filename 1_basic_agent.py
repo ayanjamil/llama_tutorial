de4f4978531e387d775d4d84a03943e7ddcece1a
ayanjamil <ayanjamil00@gmail.com>
2025-08-27 15:27:29 +0530

    chores: trying gemini

--- 1_basic_agent.py
+++ 1_basic_agent.py
@@ -1,7 +1,9 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from llama_index.llms.openai import OpenAI
+# from llama_index.llms.openai import OpenAI
+from llama_index.llms.google_genai import GoogleGenAI
+
 from llama_index.core.agent.workflow import AgentWorkflow
 
 def multiply(a: float, b: float) -> float:
@@ -12,7 +14,12 @@ def add(a: float, b: float) -> float:
     """Add two numbers and returns the sum"""
     return a + b
 
-llm = OpenAI(model="gpt-4o-mini")
+# llm = OpenAI(model="gpt-4o-mini")
+
+llm = GoogleGenAI(
+    model="gemini-2.0-flash",
+    # api_key="some key",  # uses GOOGLE_API_KEY env var by default
+)
 
 workflow = AgentWorkflow.from_tools_or_functions(
     [multiply, add],
@@ -27,3 +34,6 @@ async def main():
 if __name__ == "__main__":
     import asyncio
     asyncio.run(main())
+
+
+    
--- 2_tools.py
+++ 2_tools.py
@@ -1,7 +1,9 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from llama_index.llms.openai import OpenAI
+# from llama_index.llms.openai import OpenAI
+from llama_index.llms.google_genai import GoogleGenAI
+
 from llama_index.core.agent.workflow import AgentWorkflow
 from llama_index.tools.yahoo_finance import YahooFinanceToolSpec
 
@@ -13,10 +15,16 @@ def add(a: float, b: float) -> float:
     """Add two numbers and returns the sum"""
     return a + b
 
-llm = OpenAI(model="gpt-4o-mini")
+# llm = OpenAI(model="gpt-4o-mini")
+llm = GoogleGenAI(
+    model="gemini-2.0-flash",
+    # api_key="some key",  # uses GOOGLE_API_KEY env var by default
+)
 
 finance_tools = YahooFinanceToolSpec().to_tool_list()
+print ("FINANCIAL TOOLS ARE: ", finance_tools)
 finance_tools.extend([multiply, add])
+# we added more additonal capabiliteis to the finance_tool 
 
 workflow = AgentWorkflow.from_tools_or_functions(
     finance_tools,
